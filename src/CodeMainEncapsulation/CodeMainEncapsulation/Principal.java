b05a5486bfc62efea3d59f49920c0678ac484da5
Joseph Rios Henao <joseph232005@gmail.com>
2020-10-24 21:47:23 -0500

    Commit Inicial Modificaciones

---
+++ src/CodeMainEncapsulation/CodeMainEncapsulation/Principal.java
@@ -0,0 +1,14 @@
+
+package CodeMainEncapsulation;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, 
+                     "  Creado por"
+                + "\n  Joseph Rios H"
+                + "\n      10-2");
+        Actividad3Frame.main(args);
+    }
+}
