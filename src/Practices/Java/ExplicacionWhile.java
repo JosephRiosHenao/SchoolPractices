d006782b149c86a4e17f6360cdeeb5e6d9d832a4
Joseph Rios Henao <joseph232005@gmail.com>
2020-11-20 16:07:01 -0500

    Ajustes

--- src/Practices/Java/ExplicacionWhile.java
+++ src/Practices/Java/ExplicacionWhile.java
@@ -1,7 +1,18 @@
 package Practices.Java;
 
+import javax.swing.JOptionPane;
+
 public class ExplicacionWhile{
     public static void main (String[] args){
+        int Opcion;
         boolean Ciclo = true;
+        while (Ciclo){
+            Opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
+            "1) Promedio 1\n"
+            +"2) Promedio 2\n"
+            +"3) Salir\n"
+            ));
+
+        }
     }
 }
