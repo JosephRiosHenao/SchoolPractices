3fdea2132f99f31bc069c829217a72241a8360ff
Joseph Rios Henao <joseph232005@gmail.com>
2020-12-20 15:53:55 -0500

    Añadiendo mas tipos

--- src/Practices/C/Practice 8 (Longitud).c
+++ src/Practices/C/Practice 8 (Longitud).c
@@ -2,9 +2,13 @@
 
 int main()
 {
-    int Numeros = 1234;
+    int Enteros = 1234;
+    float Flotantes = 10.56f;
+    double DobleFlotantes = 28.523f;
     char Caracteres[5] = "Cinco";
-    printf("Longitud de Numeros: %d\n",sizeof(Numeros));
+    printf("Longitud de Flotantes: %f\n",sizeof(Flotantes));
+    printf("Longitud de Doubles: %f\n",sizeof(DobleFlotantes));
+    printf("Longitud de Enteros: %d\n",sizeof(Enteros));
     printf("Longitud de Caracteres: %d\n",sizeof(Caracteres));
     return 0;
 }
