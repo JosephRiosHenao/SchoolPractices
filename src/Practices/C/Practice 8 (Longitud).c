307f5c71c69774dd30b291b9a0b0d35b7c5b781f
Joseph Rios Henao <joseph232005@gmail.com>
2020-12-20 15:46:52 -0500

    Practica con longitudes

---
+++ src/Practices/C/Practice 8 (Longitud).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int Numeros = 1234;
+    char Caracteres[5] = "Cinco";
+    printf("Longitud de Numeros: %d\n",sizeof(Numeros));
+    printf("Longitud de Caracteres: %d\n",sizeof(Caracteres));
+    return 0;
+}
