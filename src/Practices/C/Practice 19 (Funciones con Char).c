46f5d46d5c05bbbcbfbc4fa0ca16a6fe204941bc
Joseph Rios Henao <joseph232005@gmail.com>
2020-12-23 21:34:14 -0500

    Funcion strlent funcionando..

--- src/Practices/C/Practice 19 (Funciones con Char).c
+++ src/Practices/C/Practice 19 (Funciones con Char).c
@@ -5,35 +5,28 @@
 int main()
 {
     int Eleccion;
-    char s1;
-    const char s2 = "Nice";
+    char s1[30];
+    char s2[30];
+    printf("Digite el valor de s1\n");
+    scanf("%s",&s1);
+    printf("Digite el valor de s2\n");
+    scanf("%s",&s2);
+    system("clear");
     while (true)
     {
         system("clear");
-        printf("S1:%s S2:%s\nstrcpy:%s\nstrcat:%s\nstrlen:%d\nstrcmp:%d\n1.Devolver\n2.Concatenar\n3.Longitud\n4.ComparadorBIT\n5.CambiarCadenas\n6.Salir\n",s1,s2,strcpy,strcat,strlen,);
+        printf("String 1:%s\nLongitud:%d\nString 2:%s\nLongitud:%d\n1.Cambiar Strings\n2.Salir\n",s1,strlen(s1),s2,strlen(s2));
         scanf("%d",&Eleccion);
         system("clear");
         switch (Eleccion)
         {
             case 1:
-                char *strcpy(*s1,*s2);
+                printf("Digite el valor de s1\n");
+                scanf("%s",&s1);
+                printf("Digite el valor de s2\n");
+                scanf("%s",&s2);
             break;
             case 2:
-                char *strcat(*s1,*s2);
-            break;
-            case 3:
-                int strien(const char *s1);
-            break;
-            case 4:
-                int strcmp(const char *s1,const char *s2);
-            break;
-            case 5:
-                printf("Digite el valor de s1\n");
-                scanf("%d",&s1);
-                printf("Digite el valor de s2\n");
-                scanf("%d",&s2);
-            break;
-            case 6:
                 exit(1);
             break;
             default:
