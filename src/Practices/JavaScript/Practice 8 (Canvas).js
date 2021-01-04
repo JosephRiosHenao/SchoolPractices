c4478bd13b9556c33ceeeacd68beead4b735a2ea
Joseph Rios Henao <joseph232005@gmail.com>
2021-01-04 15:09:03 -0500

    Terminando formacion de lineas

--- src/Practices/JavaScript/Practice 8 (Canvas).js
+++ src/Practices/JavaScript/Practice 8 (Canvas).js
@@ -1,28 +1,21 @@
 var d = document.getElementById("Lienzo");
 var lienzo = d.getContext("2d"); 
-
-var pasos = 50;
+var pasos = 250;
 var PasoFinal = 0;
 var X1 = 0, Y1 = 0, X2 = 500, Y2 = 500;
-
-for (let i = 0; i <= 500; i+=50) {
-    console.warn("Posiciones iniciales ("+X1+","+0+")\nPosiciones finales ("+250+","+500+")");
-    dibujarLinea("black",X1,0,250,500);
+for (let i = 0; i <= 500; i+=(pasos/2)) {
+    dibujarLinea("black",X1,Y1,X2,Y2);
     if (X1 != 500){
         X1 += pasos;
-    }else{
+    }else if (Y1 != 500){
         Y1 += pasos;
     }
-
+    if (X2 != 0){
+        X2 -= pasos;
+    }else if (Y2 != 0){
+        Y2 -= pasos;
+    }
 }
-
-/*
-dibujarLinea("black",0,0,500,500);
-dibujarLinea("red",250,0,250,500);
-dibujarLinea("red",0,500,500,0);
-*/
-
-
 function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal){
     console.log("Posiciones iniciales ("+xInicial+","+yInicial+")\nPosiciones finales ("+xFinal+","+yFinal+")");
     lienzo.beginPath();
