cf44baa8ca3c2c52ca49077c0ac96315845cb519
Joseph Rios Henao <joseph232005@gmail.com>
2021-01-03 19:27:09 -0500

    Practicando con comentarios de trazabilidad

--- src/Practices/JavaScript/Practice 6 (Ruta).js
+++ src/Practices/JavaScript/Practice 6 (Ruta).js
@@ -1,3 +1,4 @@
 var ruta = window.location;
 document.write("Estas en: "+ruta);
-console.log(navigator);
+console.log(console.error("Fallo de procesamiento"));
+console.log(console.warn("Registro de trazabilidad"));
