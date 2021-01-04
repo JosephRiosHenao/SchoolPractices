849452129f89665cc99d0a731a3347ae083ef086
Joseph Rios Henao <joseph232005@gmail.com>
2021-01-03 19:29:45 -0500

    Organizando ejercicios

--- src/Practices/JavaScript/Practice 6 (Ruta).js
+++ src/Practices/JavaScript/Practice 6 (Ruta).js
@@ -1,4 +1,3 @@
 var ruta = window.location;
 document.write("Estas en: "+ruta);
-console.log(console.error("Fallo de procesamiento"));
-console.log(console.warn("Registro de trazabilidad"));
+
---
+++ src/Practices/JavaScript/Practice 7 (Registro en consola).js
@@ -0,0 +1,2 @@
+console.log(console.error("Fallo de procesamiento"));
+console.log(console.warn("Registro de trazabilidad"));
