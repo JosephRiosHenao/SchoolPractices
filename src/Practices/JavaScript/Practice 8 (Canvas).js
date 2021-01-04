7d50a7ca79fe9bb3843e9a2c23f976c382236bea
Joseph Rios Henao <joseph232005@gmail.com>
2021-01-04 14:28:07 -0500

    Manejando coordenadas en Canvas JS

--- src/Practices/JavaScript/Practice 8 (Canvas).js
+++ src/Practices/JavaScript/Practice 8 (Canvas).js
@@ -1,9 +1,34 @@
 var d = document.getElementById("Lienzo");
 var lienzo = d.getContext("2d"); 
 
-lienzo.beginPath();
-lienzo.strokeStyle = "red";
-lienzo.moveTo(100,100);
-lienzo.lineTo(200,200);
-lienzo.stroke();
-lienzo.closePath();
+var pasos = 50;
+var PasoFinal = 0;
+var X1 = 0, Y1 = 0, X2 = 500, Y2 = 500;
+
+for (let i = 0; i <= 500; i+=50) {
+    console.warn("Posiciones iniciales ("+X1+","+0+")\nPosiciones finales ("+250+","+500+")");
+    dibujarLinea("black",X1,0,250,500);
+    if (X1 != 500){
+        X1 += pasos;
+    }else{
+        Y1 += pasos;
+    }
+
+}
+
+/*
+dibujarLinea("black",0,0,500,500);
+dibujarLinea("red",250,0,250,500);
+dibujarLinea("red",0,500,500,0);
+*/
+
+
+function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal){
+    console.log("Posiciones iniciales ("+xInicial+","+yInicial+")\nPosiciones finales ("+xFinal+","+yFinal+")");
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xInicial,yInicial);
+    lienzo.lineTo(xFinal,yFinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
