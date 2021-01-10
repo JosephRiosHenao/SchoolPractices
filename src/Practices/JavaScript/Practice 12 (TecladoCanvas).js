2066f82ba3367fa9e3b0900b2694a439d3fdb473
Joseph Rios Henao <joseph232005@gmail.com>
2021-01-09 22:12:52 -0500

    Agregando detector de teclado

--- src/Practices/JavaScript/Practice 12 (TecladoCanvas).js
+++ src/Practices/JavaScript/Practice 12 (TecladoCanvas).js
@@ -1,1 +1,15 @@
-alert("Hola")
+var teclado = {
+    
+}
+
+document.addEventListener("keydown",dibujarTeclado)
+
+function dibujarTeclado(Tecla) {
+    console.log(Tecla.keyCode)
+}
+/*
+    w = 87
+    a = 65
+    s = 83
+    d = 68
+*/
