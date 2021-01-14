b48df9184e119ba1ad91dd3aba82040b14db02c0
Joseph Rios Henao <joseph232005@gmail.com>
2021-01-14 15:05:43 -0500

    Termianando reconocimiento de teclado JS

--- src/Practices/JavaScript/Practice 12 (TecladoCanvas).js
+++ src/Practices/JavaScript/Practice 12 (TecladoCanvas).js
@@ -1,12 +1,28 @@
 var teclado = {
-    W = 87,
-    A = 65,
-    S = 83,
-    D= 68
+    W: 87,
+    A: 65,
+    S: 83,
+    D: 68
 }
 
 document.addEventListener("keydown",dibujarTeclado)
 
-function dibujarTeclado(Tecla) {
-    console.log(Tecla.keyCode)
+function dibujarTeclado(Teclas) {
+    switch (Teclas.keyCode) {
+        case teclado.W:
+            console.log("Arriba!");
+        break;
+
+        case teclado.S:
+            console.log("Abajo!");
+        break;
+
+        case teclado.A:
+            console.log("Izquierda!");
+        break;
+
+        case teclado.D:
+            console.log("Derecha!");
+        break;
+    }
 }
