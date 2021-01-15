d8ba551e762c3993d2aa342d96275f3dd3fb4f98
Joseph Rios Henao <joseph232005@gmail.com>
2021-01-15 07:13:47 -0500

    Añadiendo funcionalidad

--- src/Practices/JavaScript/Practice 12 (TecladoCanvas).js
+++ src/Practices/JavaScript/Practice 12 (TecladoCanvas).js
@@ -20,25 +20,37 @@ function dibujarTeclado(Teclas) {
     switch (Teclas.keyCode) {
         case teclado.W:
             console.log("Arriba!");
+            Posicion.Final.Y -= PASOS;
+            DibujarLinea("black",Posicion.Inicial.X,Posicion.Inicial.Y,Posicion.Final.X,Posicion.Final.Y);
+            Posicion.Inicial.Y -= PASOS;
         break;
 
         case teclado.S:
             console.log("Abajo!");
+            Posicion.Final.Y += PASOS;
+            DibujarLinea("black",Posicion.Inicial.X,Posicion.Inicial.Y,Posicion.Final.X,Posicion.Final.Y);
+            Posicion.Inicial.Y += PASOS;
         break;
 
         case teclado.A:
             console.log("Izquierda!");
+            Posicion.Final.X -= PASOS;
+            DibujarLinea("black",Posicion.Inicial.X,Posicion.Inicial.Y,Posicion.Final.X,Posicion.Final.Y);
+            Posicion.Inicial.X -= PASOS;
         break;
 
         case teclado.D:
             console.log("Derecha!");
+            Posicion.Final.X += PASOS;
+            DibujarLinea("black",Posicion.Inicial.X,Posicion.Inicial.Y,Posicion.Final.X,Posicion.Final.Y);
+            Posicion.Inicial.X += PASOS;
         break;
     }
 }
 
 function DibujarLinea(Color,X1,Y1,X2,Y2){
     Pincel.beginPath();
-    Pincel.strokeStyle = color;
+    Pincel.strokeStyle = Color;
     Pincel.moveTo(X1,Y1);
     Pincel.lineTo(X2,Y2);
     Pincel.stroke();
