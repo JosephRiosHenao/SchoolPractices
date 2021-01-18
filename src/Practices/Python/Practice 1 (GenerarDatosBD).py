b3c5ac19a810f6089a22164cd72e9864a7e3a647
Joseph Rios Henao <joseph232005@gmail.com>
2021-01-17 20:52:24 -0500

    Ampliando script

--- src/Practices/Python/Practice 1 (GenerarDatosBD).py
+++ src/Practices/Python/Practice 1 (GenerarDatosBD).py
@@ -13,7 +13,7 @@ def Generated(Almacen,Tabla):
     NombreLista = (["Carmen","Pedro","Joaquín","Jesús","María","Cristiano","Andres","Gerad","Ana","Eric","Eva","Hugo","Ivan","Juan","Laura","Leo"])
     CruceLista = (["Cr","Cll"])
     SubCruceLista = (["A","B","C","D","E","F",""])
-    CiudadLista = (["Popayan","Timbio","Cali","Barranquilla","Medellin","Pereira","Guacari","Cartagena","Bogota"])
+    CiudadLista = (["Popayan","Timbio","Cali","Cucuta","Medellin","Pereira","Guacari","Cartagena","Bogota"])
     ProductoLista = (["Chocolates","Licor","Dulces","Jugo","Cepillo","Alcohol","Golosinas","Helado","Abientador","Carne","Pollo","Pavo","Costilla","Pimienta","Cayena","Sal","Azucar","Cafe","Frijoles","Yuca","Platano","Papa","Guisantes","Tomate","Cebolla","Mantequilla","Harina","Huevo","Queso","Jamon","Mermelada","Fresa","Uvas","Melocoton"])
     for ID in range(1000):
         Nombre = random.choice(NombreLista)
@@ -24,11 +24,18 @@ def Generated(Almacen,Tabla):
         NumeroDeCasa = random.randint(1,100)
         Producto = random.choice(ProductoLista)
         Ciudad = random.choice(CiudadLista)
+        CodigoProducto = random.randint(0,1000)
+        Cantidad = random.randint(0,100)
+        PrecioIndividual = random.randint(1000,10000)
+        Precio = PrecioIndividual * Cantidad
         Dirrecion = f"{Cruce} {NumeroDeCruce}{SubCruce} #{NumeroDeCruceContrario}-{NumeroDeCasa}"
-        Almacen += f"\nINSERT INTO {Tabla} VALUES (\"{ID}\",\"{Nombre}\",\"{Dirrecion}\",\"{Ciudad}\");"
+        #Almacen += f"\nINSERT INTO {Tabla} VALUES (\"{ID}\",\"{Nombre}\",\"{Dirrecion}\",\"{Ciudad}\");"
         #Almacen += f"\nINSERT INTO {Tabla} VALUES (\"{ID}\",\"{Producto}\");"
+        Almacen += f"\nINSERT INTO {Tabla} VALUES (\"{ID}\",\"{CodigoProducto}\",\"{Cantidad}\",\"{Precio}\");"
+
     print("Finalizado")
     Copy(Almacen)
 
 print("Iniciando")
-Generated(Almacen,"cliente")
+Generated(Almacen,"item_ventas")
+#ITEM_VENTAS(Número_Factura, codigo_producto,cantidad,precio) VENTAS(numero_factura, codigo_cliente,fecha)
