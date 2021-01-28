ee8f24bf6f674d30535f5eb50c6aff3956b2a209
Joseph Rios Henao <joseph232005@gmail.com>
2021-01-27 21:13:56 -0500

    Iniciando nuevo punto

--- src/Practices/Flutter/practice_2_VScode/prueba_vscode/android/app/src/main/kotlin/com/example/prueba_vscode/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.prueba_vscode
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ src/Practices/Java/Practice_4_EjerciciosDoWhile.java
@@ -0,0 +1,11 @@
+package Practices.Java;
+
+public class Practice_4_EjerciciosDoWhile {
+
+    public static void main(String[] args) {
+        
+    }
+    public static void Punto11(){
+        
+    }
+}
