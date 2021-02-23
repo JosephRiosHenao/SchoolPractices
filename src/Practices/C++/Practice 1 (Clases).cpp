f7f54e51e9f03b63a65a21b81cc40af98a6da393
Joseph Rios Henao <joseph232005@gmail.com>
2021-02-23 15:46:24 -0500

    Practicando con cpp

---
+++ src/Practices/C++/Practice 1 (Clases).cpp
@@ -0,0 +1,8 @@
+#include "Practice 2 (Header).h";
+
+
+int main(){
+    foo a;
+    bar b;
+    b.provar;
+}
---
+++ src/Practices/C++/Practice 3 (LogicHeader).cpp
@@ -0,0 +1,26 @@
+#include "Practice 2 (Header).h"
+
+foo::foo(){
+    var = 0;
+    pvar = 0;
+}
+
+void foo::setPvar(int i){
+    pvar = i
+}
+
+int foo::getPvar(){
+    return pvar;
+}
+
+void foo::algo(){
+    provar = 1;
+}
+
+bar::bar(){
+    provar = 1;
+}
+
+void bar::algo(){
+    provar = 4;
+}
