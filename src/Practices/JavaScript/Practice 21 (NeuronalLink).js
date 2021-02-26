b897bf8b41566aa16e55b674aa219fd1889f596f
Joseph Rios Henao <joseph232005@gmail.com>
2021-02-26 15:12:10 -0500

    avanzando con udemy

--- src/Practices/JavaScript/Practice 21 (NeuronalLink).js
+++ src/Practices/JavaScript/Practice 21 (NeuronalLink).js
@@ -2,6 +2,17 @@ colorSelectorObject = document.getElementById("colorSelector");
 colorSelectorObject.addEventListener("input",update,false);
 colorSelectorObject.addEventListener("change",update,false);
 function update(){
-    console.log(colorSelectorObject.value);
+    console.log(hexToRGB(colorSelectorObject.value).r);
+    console.log(hexToRGB(colorSelectorObject.value).g);
+    console.log(hexToRGB(colorSelectorObject.value).b);
+    console.log(hexToRGB(colorSelectorObject.value));
 }
-//setInterval('update()',500)
+function  hexToRGB(hex){
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+var net = new NeuralNetwork();
