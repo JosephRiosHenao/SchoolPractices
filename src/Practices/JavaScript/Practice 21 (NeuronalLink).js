d5dc41bfdec4e0448f03492a73b51e7146b89ec3
Joseph Rios Henao <joseph232005@gmail.com>
2021-02-27 15:47:03 -0500

    Termine Red Neuronal

--- src/Practices/JavaScript/Practice 21 (NeuronalLink).js
+++ src/Practices/JavaScript/Practice 21 (NeuronalLink).js
@@ -1,12 +1,31 @@
-colorSelectorObject = document.getElementById("colorSelector");
+var net = new brain.NeuralNetwork();
+
+
+var colorSelectorObject = document.getElementById("colorSelector");
+var backTitle = document.getElementById("sitio");
+
 colorSelectorObject.addEventListener("input",update,false);
 colorSelectorObject.addEventListener("change",update,false);
+
 function update(){
-    console.log(hexToRGB(colorSelectorObject.value).r);
-    console.log(hexToRGB(colorSelectorObject.value).g);
-    console.log(hexToRGB(colorSelectorObject.value).b);
-    console.log(hexToRGB(colorSelectorObject.value));
+    // console.log(hexToRGB(colorSelectorObject.value).r);
+    // console.log(hexToRGB(colorSelectorObject.value).g);
+    // console.log(hexToRGB(colorSelectorObject.value).b);
+    // console.log(hexToRGB(colorSelectorObject.value));
+    backTitle.style.background = colorSelectorObject.value;
+    var inputColors = {
+        red: (hexToRGB(colorSelectorObject.value).r)/255,
+        green: (hexToRGB(colorSelectorObject.value).g)/255,
+        blue: (hexToRGB(colorSelectorObject.value).b)/255
+    };
+
+    var result = net.run(inputColors);
+    console.log(result);
+    var textColor = result.color > .5 ? "white" : "black";
+    backTitle.style.color = textColor;
 }
+
+
 function  hexToRGB(hex){
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
@@ -15,4 +34,17 @@ function  hexToRGB(hex){
         b: parseInt(result[3], 16)
     } : null;
 }
-var net = new NeuralNetwork();
+
+net.train([
+    //fondo negro, color blanco
+    { input:{red:0,green:0,blue:0}, output:{color:1}},
+    //fondo blanco, color negro
+    { input:{red:1,green:1,blue:1}, output:{color:0}}
+])
+/*
+function NeuralNetwork(red,green,blue){
+    var Salida;
+
+    return Salida;
+}
+*/
