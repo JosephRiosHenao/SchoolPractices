a2095fe46812a6d29101a09ee6f64ad732a5f75d
Joseph Rios Henao <joseph232005@gmail.com>
2021-03-11 11:49:28 -0500

    Testing particles.js

--- src/Practices/JavaScript/Practice 24 (MenuSticky).js
+++ src/Practices/JavaScript/Practice 24 (MenuSticky).js
@@ -3,5 +3,24 @@ const btnSwitch = document.querySelector("#switch")
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
+
+    //LOCAL STORAGE
+    if (document.body.classList.contains('dark')){
+        localStorage.setItem('dark-theme','true')
+    }else{
+        localStorage.setItem('dark-theme','false')
+    }
 });
 
+//LOCAL STORAGE INICIAL
+if (localStorage.getItem('dark-theme') === 'true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+}else{
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+}
+
+particlesJS.load('particles-js', '../JSON/particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
