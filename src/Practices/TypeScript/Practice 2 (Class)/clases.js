d7f3272ec028b866bea9214fe41db9b4ea1ba99d
Joseph Rios Henao <joseph232005@gmail.com>
2021-03-28 10:44:50 -0500

    finish practice 2 with class in TypeScript

--- src/Practices/TypeScript/Practice 2 (Class)/clases.js
+++ src/Practices/TypeScript/Practice 2 (Class)/clases.js
@@ -1,1 +1,16 @@
 "use strict";
+class Persona {
+    constructor(nombre) {
+        this.nick = "juan";
+        this.nick = nombre;
+    }
+    getNick() {
+        return this.nick;
+    }
+    static metodStatic() {
+        return 10;
+    }
+}
+let persona = new Persona("Carlos");
+alert(persona.getNick());
+alert(Persona.metodStatic());
