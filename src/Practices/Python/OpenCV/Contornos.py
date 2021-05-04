f1056951ef0853812bc978b5184c894f37df2ac3
Joseph Rios Henao <joseph232005@gmail.com>
2021-05-03 19:30:51 -0500

    fix issues

--- src/Practices/Python/OpenCV/Contornos.py
+++ src/Practices/Python/OpenCV/Contornos.py
@@ -1,6 +1,6 @@
 import cv2
 
-path = "IMG/FigurasGeometricasJPG.jpg"
+path = "D:\JOSEPH\GIT\SchoolPractices\src\Practices\Python\OpenCV\IMG\FigurasGeometricasJPG.jpg"
 imagen = cv2.imread(path)
 
 cv2.imshow("image",imagen)
