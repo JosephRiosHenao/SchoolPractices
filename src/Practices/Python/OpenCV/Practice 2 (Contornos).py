fe8097e5e1fac0b04ca856d72cf819a2a1f64b14
Joseph Rios Henao <joseph232005@gmail.com>
2021-05-05 15:32:06 -0500

    practice with contorns, tresholder and more

--- src/Practices/Python/OpenCV/Practice 2 (Contornos).py
+++ src/Practices/Python/OpenCV/Practice 2 (Contornos).py
@@ -1,8 +1,19 @@
 import cv2
 
-path = "D:\JOSEPH\GIT\SchoolPractices\src\Practices\Python\OpenCV\IMG\Figuras Geometricas.jpg"
+path = "D:\JOSEPH\GIT\SchoolPractices\src\Practices\Python\OpenCV\IMG\Figuras Geometricas.png"
+
 imagen = cv2.imread(path)
 
+gray = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+
+tipoUmbral,umbral = cv2.threshold(gray,225,255,cv2.THRESH_BINARY)
+
+contorno, jerarquia = cv2.findContours(umbral,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.drawContours(imagen,contorno,-1,(0,255,0),4)
+
+# cv2.imshow("OUTPUT",umbral)
+# cv2.imshow("OUTPUT",gray)
 cv2.imshow("OUTPUT",imagen)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
