cfc2a6a94c48c39b5e19842db16d268e7e2a8d90
Joseph Rios Henao <joseph232005@gmail.com>
2021-05-12 14:59:46 -0500

    add contours and find count in moneys

--- src/Practices/Python/OpenCV/Practice 3  (ContadorMonedas).py
+++ src/Practices/Python/OpenCV/Practice 3  (ContadorMonedas).py
@@ -5,11 +5,19 @@ original = cv2.imread("D:\JOSEPH\GIT\SchoolPractices\src\Practices\Python\OpenCV
 
 gris = cv2.cvtColor(original,cv2.COLOR_BGR2GRAY)
 
-blur = cv2.GaussianBlur(gris,(7,7),0)
+blur = cv2.GaussianBlur(gris,(3,3),0)
 
-canny = cv2.Canny(blur,60,100)
+canny = cv2.Canny(blur,80,100)
 
+kernel = np.ones((6,6),np.uint8)
 
-cv2.imshow("Output",canny)
+cierre = cv2.morphologyEx(canny,cv2.MORPH_CLOSE,kernel)
+
+contorno, jerarquia = cv2.findContours(cierre.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+print("Monedas encontradas: {}".format(len(contorno)))
+
+cv2.drawContours(original,contorno,-1,(255,255,0),2)
+
+cv2.imshow("Output",original)
 cv2.waitKey(0)
 cv2.destroyAllWindows() 
