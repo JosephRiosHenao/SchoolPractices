3d37fb714cdbca2ca26db9420f222f58385d686d
Joseph Rios Henao <joseph232005@gmail.com>
2021-08-23 21:20:54 -0500

    adding countries array

---
+++ JavaScript/Practice 34 (Callbacks).js
@@ -0,0 +1,73 @@
+let countries = [
+    {
+        id: 1,
+        name: 'Afghanistan',
+        code: 'AF'
+    },
+    {
+        id: 2,
+        name: 'Albania',
+        code: 'AL'
+    },
+    {
+        id: 3,
+        name: 'Algeria',
+        code: 'DZ'
+    },
+    {
+        id: 4,
+        name: 'Andorra',
+        code: 'AD'
+    },
+    {
+        id: 5,
+        name: 'Angola',
+        code: 'AO'
+    },
+    {
+        id: 6,
+        name: 'Anguilla',
+        code: 'AI'
+    },
+    {
+        id: 7,
+        name: 'Antarctica',
+        code: 'AQ'
+    },
+    {
+        id: 8,
+        name: 'Antigua and Barbuda',
+        code: 'AG'
+    },
+    {
+        id: 9,
+        name: 'Argentina',
+        code: 'AR'
+    },
+    {
+        id: 10,
+        name: 'Armenia',
+        code: 'AM'
+    },
+    {
+        id: 11,
+        name: 'Aruba',
+        code: 'AW'
+    },
+    {
+        id: 12,
+        name: 'Australia',
+        code: 'AU'
+    },
+    {
+        id: 13,
+        name: 'Austria',
+        code: 'AT'
+    },
+    {
+        id: 14,
+        name: 'Awupaki Island',
+        code: 'AI'
+    }
+]
+
