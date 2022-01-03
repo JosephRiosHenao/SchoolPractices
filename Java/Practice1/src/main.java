d9d40f3b4e68917d493dedfcd7875178953d060c
Joseph Rios Henao <joseph232005@gmail.com>
2022-01-03 14:10:22 -0500

    fix

--- Java/Practice1/src/main.java
+++ Java/Practice1/src/main.java
@@ -1,5 +1,6 @@
 
 import javax.swing.JOptionPane;
+import java.util.Random;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,10 +15,8 @@ import javax.swing.JOptionPane;
 public class main {
     
     public static void main(String[] args) {
-        while(true){
             int menuOtp = menu();
             filter(menuOtp);
-        }
     }
     
     public static int menu(){
@@ -48,9 +47,83 @@ public class main {
                 activity5();
                 break;
             case 6:
-                
-                
-                     
+                int socialMedia = Integer.parseInt(JOptionPane.showInputDialog(null,"1.TWITTER\n2.WHATSAPP\n3.FACEBOOK"));
+                switch (socialMedia){
+                    case 1:
+                        JOptionPane.showMessageDialog(null,"Es la plataforma de comunicación en tiempo real, más importante actualmente. El uso de hashtags facilita el alcance orgánico en esta plataforma. Información sintetizada en 280 caracteres. No es tan eficaz en términos publicitarios como las otras plataformas");
+                        break;
+                    case 2:
+                        JOptionPane.showMessageDialog(null,"Una plataforma que cuenta con un gran número de visualizaciones/impresiones diarias. Tiene una mayor cercanía con el usuario, al usar su número telefónico como requisito para crear una cuenta en la aplicación. Se está implementando la herramienta WhatsApp Business, que permite revisar estadísticas o automatizar ciertos procesos con clientes.");
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null,"Gran variedad de contenidos y formatos, con una herramienta muy potente de segmentación y analítica. Presente muy buenos ratios de conversión y leads. Sin embargo, el alcance orgánico se está reduciendo con el nuevo algoritmo e impide crecer de manera natural a las empresas, obligando a invertir en su plataforma de publicidad (Facebook Ads).");
+                }
+                break;
+            case 7:
+                while(true){
+                    float n3 = Float.parseFloat(JOptionPane.showInputDialog(null,"Digite un numero, si es 0 finaliza el bucle"));
+                    if (n3==0) break;
+                    JOptionPane.showMessageDialog(null, "Si da 0 es par, si no es impar \n El numero es: "+(n3%2));
+                }
+                break;
+            case 8:
+                int maxEmployed = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite el numero de empleados"));
+                int[] nEmployes = new int[maxEmployed];
+                int minProfit = 0, maxProfit = 0, totalProfit = 0;
+                String data2 = "";
+                Random generadorAleatorios = new Random();
+                for (int i = 0; i < nEmployes.length; i++) {
+                    nEmployes[i] = 700000+generadorAleatorios.nextInt(1500000);;
+                    data2 += (i+1)+") Sueldo: "+ nEmployes[i]+"\n";
+                    totalProfit += nEmployes[i];
+                    if (nEmployes[i]>700000 && nEmployes[i]<999999) minProfit++;
+                    else maxProfit++;
+                }
+                JOptionPane.showMessageDialog(null, "LISTA DE EMPLEADOS\n\n"+data2);
+                JOptionPane.showMessageDialog(null, "TOTAL DE EMPLEADOS: "+nEmployes.length+"\nEMPLEADOS CON SUELDO MINIMO: "+minProfit+"\nEMPLEADOS CON SUELDO MAXIMO: "+maxProfit+"TOTAL DE SUELDO: "+totalProfit+"\n");
+                break;
+            case 9:
+                float[] nums = new float[8];
+                float sumTotal = 0;
+                float mulTotal = 0;
+                for (int i = 0; i < nums.length; i++) {
+                    nums[i] = Float.parseFloat(JOptionPane.showInputDialog(null,"Digite el #"+(i+1)+": "));
+                    sumTotal += nums[i];
+                    mulTotal += nums[i];
+                }
+                JOptionPane.showMessageDialog(null, "La suma total fue: "+sumTotal+"\nLa multiplicacion total fue: "+mulTotal);
+                break;
+            case 10:
+                float numMax = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite el numero maximo"));
+                float acum = 0;
+                for (int i = 0; i < numMax; i+=3) {
+                    acum += i;
+                    JOptionPane.showMessageDialog(null, "El ciclo es el #"+i+" y el total sumado es: "+ acum);
+                }
+                break;
+            case 11:
+                int menuSelect2 = 0;
+                switch (menuSelect2){
+                    case 1:
+                        JOptionPane.showMessageDialog(null, "                    nEmployes[i] = 700000+generadorAleatorios.nextInt(1500000);;\n" +
+"                    data2 += (i+1)+\") Sueldo: \"+ nEmployes[i]+\"\\n\";\n" +
+"                    totalProfit += nEmployes[i];\n" +
+"                    if (nEmployes[i]>700000 && nEmployes[i]<999999) minProfit++;\n" +
+"                    else maxProfit++;");
+                        break;
+                    case 2:
+                        JOptionPane.showMessageDialog(null, "\"                    if (nEmployes[i]>700000 && nEmployes[i]<999999) minProfit++;\\n\" +\n" +
+"\"                    else maxProfit++;");
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "                for (int i = 0; i < nEmployes.length; i++) {\n" +
+"                    nEmployes[i] = 700000+generadorAleatorios.nextInt(1500000);;\n" +
+"                    data2 += (i+1)+\") Sueldo: \"+ nEmployes[i]+\"\\n\";\n" +
+"                    totalProfit += nEmployes[i];\n" +
+"                    if (nEmployes[i]>700000 && nEmployes[i]<999999) minProfit++;\n" +
+"                    else maxProfit++;\n" +
+"                }");
+                }
         }
     }
     
