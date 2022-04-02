4373c8023cc0d3ede1f967d904a598dd2b5c0962
Joseph Rios Henao <joseph232005@gmail.com>
2022-04-02 16:36:40 -0500

    new project

---
+++ Java/RestaurantMenu/src/initClass.java
@@ -0,0 +1,20 @@
+
+import pages.login;
+import pages.shopping_cart;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RiosHenao
+ */
+public class initClass {
+    public static void main(String[] args) {
+        login.main(args);
+        //shopping_cart.main(args);
+    }
+}
